8c035b4b2dc94c03d95f8fc5a28d046a69055c5a
Christopher Breit <christopher.breit@charter.com>
2020-04-27 15:35:56 -0600

    updates to make app a webcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,15 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [],
+  entryComponents: [AppComponent],
 })
-export class AppModule { }
+
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const appElement = createCustomElement(AppComponent, { injector: this.injector});
+    customElements.define('mfe-poc-billing', appElement);
+  }
+}
