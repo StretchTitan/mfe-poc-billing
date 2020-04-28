022498faee3b2c83c2ebc8e3d131c2963813eccc
Christopher Breit <christopher.breit@charter.com>
2020-04-28 09:54:31 -0600

    mfe app communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mfe-poc-billing';
+  @Input() state;
+  @Output() msg = new EventEmitter<any>();
+
+  clearName() {
+    this.msg.emit({ action: 'clearName' });
+  }
 }
